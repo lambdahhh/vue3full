747bdafc3a3e3f289d3eaa45f882175e5e754423
volkov.p <lambdahhh@gmail.com>
2022-08-03 15:55:46 +0300

    init commit

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,34 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div>
+    <div>
+      <button v-on:click="addLike">Like</button>
+      <button @click="addDislike">Dislike</button>
+    </div>
+    <div><strong>Кол-во лайков: {{ likes }}</strong></div>
+    <div><strong>Кол-во дизов: {{ dislikes }}</strong></div>
+  </div>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      likes: 0,
+      dislikes: 0
+    }
+  },
+  methods: {
+    addLike() {
+      this.likes += 1;
+    },
+    addDislike() {
+      this.dislikes += 1;
+    }
+  }
 }
+</script>
 
-nav {
-  padding: 30px;
-}
+<style scoped>
 
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
